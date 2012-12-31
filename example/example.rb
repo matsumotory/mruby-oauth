e704ff4e30523fda6d8031fe7409a0a3356e6b37
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2012-12-31 16:39:34 +0900

    fix example

--- example/example.rb
+++ example/example.rb
@@ -7,7 +7,7 @@ API_URL             = 'http://api.twitter.com/1.1/statuses/update.json'
 tweet       = "hello twitter world from mruby-oauht"
 ex_headers  = {'Content-Type' => 'application/x-www-form-urlencoded'}
 twitter     = OAuth.new(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-response    = twitter.post(API_URL, {:status => msg}, ex_headers)
+response    = twitter.post(API_URL, {:status => tweet}, ex_headers)
 
 if response.code.to_i == 200
   puts "tweet success: #{msg}"
