4a8f3110b2f31bd3bdfb67e7b8be95c67846e78b
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-01-01 22:58:01 +0900

    Add example

--- example/tweet_timeline.rb
+++ example/tweet_timeline.rb
@@ -2,15 +2,31 @@ CONSUMER_KEY        = ''
 CONSUMER_SECRET     = ''
 ACCESS_TOKEN        = ''
 ACCESS_TOKEN_SECRET = ''
-GET_API_URL         = 'http://api.twitter.com/1.1/statuses/user_timeline.json?count=1'
+GET_API_URL         = 'http://api.twitter.com/1/statuses/home_timeline.json?count='
 
+tweet_num = 10
+interval = 60
 twitter = OAuth.new(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-response = twitter.get(GET_API_URL)
 
-if response.code.to_i == 200
-  data = JSON::parse(response.body)
-  puts "#{data[0]["user"]["screen_name"]}'s tweet:  #{data[0]["text"]}"
-else
-  puts response.body
-  raise "Request failed: " + response.code.to_s
+new_id = 0
+old_id = 0
+
+while
+  response = twitter.get(GET_API_URL + tweet_num.to_s)
+  if response.code.to_i == 200
+    data = JSON::parse(response.body)
+    if old_id == 0 || old_id != data[0]["id"].to_i
+      new_id = data[0]["id"].to_i
+      data.reverse.each do |d|
+        if old_id < d["id"].to_i
+          puts "#{d["user"]["screen_name"]}'s tweet:  #{d["text"]}"
+        end
+      end
+      old_id = new_id
+    end
+  else
+    puts response.body
+    raise "Request failed: " + response.code.to_s
+  end
+  Random.sleep interval
 end
